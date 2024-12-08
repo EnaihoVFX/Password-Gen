e46953fb40672b96aa1b00387c0f487fa2298ffb
Enaiho <155083726+EnaihoVFX@users.noreply.github.com>
2024-12-08 10:02:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-import random
+import generator
 
-char="abcdefghijklmnopqrstuvwxyz"
-specialchar="£$%^&*@~#?/\!"
-numbers="0123456789"
-
-password= ""
-length= random.randint(10,12)
-cache_length=length
-while length>0:
-    choice= random.randint(1,2)
-    match choice:
-        case 1:
-            password+=char[random.randint(0,24)]
-        case 2:
-            password+=char[random.randint(0,24)].upper()
-    length-=1
-    password+=specialchar[random.randint(0,12)]
-    length-=1
-    password+=numbers[random.randint(0,9)]
-    length-=1
-
-print(f"Your password is {password} with a length of {cache_length} char")
+print("Enter 1 to generate a password\nEnter 2 to store a password")
+user_choice= int(input("Enter choice 1 or 2: "))
+match user_choice:
+    case 1:
+        password= generator.password()
+        print(f"{password}, length: {len(password)}")
+    case 2:
+        password= input("Enter a password to store: ")
+        username= input("Enter the username associated with the password: ")
+        generator.store(password,username)
+    case default:
+        print("Choice not valid")
